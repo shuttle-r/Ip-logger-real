c17e047eb07ae1b7e2c0da9ed4acff0b85a74987
shuttle-r <109020578+shuttle-r@users.noreply.github.com>
2022-07-22 07:50:41 +0800

    update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,2 +1,6 @@
+makefolder("vape")
+makefolder("vape/CustomModules")
+makefolder("vape/assets")
+makefolder("vape/Profiles")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 
